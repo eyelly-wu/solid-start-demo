0b81f76ed168ffd8a97a322063463fbe1f74bd53
Eyelly Wu <eyelly.wu@gmail.com>
2023-10-29 22:32:21 +0800

    init commit

---
+++ src/root.tsx
@@ -0,0 +1,50 @@
+// @refresh reload
+import { Suspense } from "solid-js";
+import {
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start";
+import { I18nProvider, useI18n } from '@i18n-pro/solid'
+import "./root.css";
+import i18nState from './i18n'
+
+function Root() {
+  const { t } = useI18n()
+  return (
+    <Html lang="en">
+      <Head>
+        <Title>SolidStart - Bare</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <A href="/">Index</A>
+            <A href="/about">About</A>
+            <Routes>
+                <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
+
+export default function Wrapper(){
+  return (
+    <I18nProvider {...i18nState}>
+      <Root />
+    </I18nProvider>
+  )
+}
